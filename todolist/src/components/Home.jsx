0364f7ffd4596a2f2d9d3bb478e1fa52d7cac900
666Deadsoul <sarinamaharjan234@gmail.com>
2024-11-23 20:31:28 +0545

    added JS to log the tasks

--- todolist/src/components/Home.jsx
+++ todolist/src/components/Home.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import TaskInput from "./TaskInput";
 import './home.css';
 
 const Home=()=>{
+    const [todolist, setTodolist] = useState([]);
+    const addTask = (taskName)=>{
+        const newTask = {taskName, checked: false};
+        setTodolist([...todolist,newTask])
+    };
+    console.log(todolist);
+
     return(
         <>
             <div className="container">
                 <h1>To-Do-List</h1>
-                <TaskInput/>
+                <TaskInput addTask={addTask}/>
                 <div className="todolist">
                     <span>To Do</span>
                     <ul className="list-items"></ul>
--- todolist/src/components/TaskInput.jsx
+++ todolist/src/components/TaskInput.jsx
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
 import "./taskinp.css";
 
-const TaskInput =()=>{
+const TaskInput =({addTask})=>{
 
     const [task, setTask] = useState('');
-    console.log(task);
+    //console.log(task);
 
     function handleInputValue(event){
         setTask(event.target.value);
     }
-
+    function handleAddTask(event){
+        event.preventDefault();
+        addTask(task);
+    }
     return(
         <>
-            <form className="inputField">
+            <form className="inputField" onSubmit={handleAddTask}>
                 <input type="text" placeholder="Add Item" onChange={handleInputValue}/>
                 <button>+</button>
             </form>
